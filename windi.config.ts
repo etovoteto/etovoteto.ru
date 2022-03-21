76f040add9e1cc54b7daa2341e98ad44a14da245
davay <fond@fondfrukt.ru>
2022-03-21 10:44:45 +0300

    bump

--- windi.config.ts
+++ windi.config.ts
@@ -5,9 +5,6 @@ import colors from 'windicss/colors'
 
 export default defineConfig({
   darkMode: 'class',
-  shortcuts: {
-    bc: 'border-gray-300 dark:border-true-gray-600',
-  },
   plugins: [
     require('windicss/plugin/scroll-snap')
   ],
@@ -23,8 +20,6 @@ export default defineConfig({
       },
       colors: {
         bg: 'var(--c-bg)',
-        gray: colors.trueGray,
-        tg: colors.trueGray,
       },
       boxShadow: {
         box: '0px 5px 15px 0px rgba(0, 0, 0, 0.35)',
